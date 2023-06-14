69fe8272c3f9352986f7b64f72eff2b795af25c9
maxdesalle <max@sablier.finance>
2023-06-14 11:23:52 +0200

    refactor: set up website for TweetNFTs

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,38 +1,32 @@
-const mode = document.getElementById('mode');
+const mode = document.getElementById("mode");
 
 if (mode !== null) {
+  window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", (event) => {
+      if (event.matches) {
+        localStorage.setItem("theme", "dark");
+        document.documentElement.setAttribute("data-dark-mode", "");
+      } else {
+        // localStorage.setItem("theme", "light");
+        // document.documentElement.removeAttribute("data-dark-mode");
+      }
+    });
 
-  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+  // mode.addEventListener('click', () => {
 
-    if (event.matches) {
+  //   document.documentElement.toggleAttribute('data-dark-mode');
+  //   localStorage.setItem('theme', document.documentElement.hasAttribute('data-dark-mode') ? 'dark' : 'light');
 
-      localStorage.setItem('theme', 'dark');
-      document.documentElement.setAttribute('data-dark-mode', '');
+  // });
 
-    } else {
+  // if (localStorage.getItem('theme') === 'dark') {
 
-      localStorage.setItem('theme', 'light');
-      document.documentElement.removeAttribute('data-dark-mode');
+  //   document.documentElement.setAttribute('data-dark-mode', '');
 
-    }
+  // } else {
 
-  })
+  //   document.documentElement.removeAttribute('data-dark-mode');
 
-  mode.addEventListener('click', () => {
-
-    document.documentElement.toggleAttribute('data-dark-mode');
-    localStorage.setItem('theme', document.documentElement.hasAttribute('data-dark-mode') ? 'dark' : 'light');
-
-  });
-
-  if (localStorage.getItem('theme') === 'dark') {
-
-    document.documentElement.setAttribute('data-dark-mode', '');
-
-  } else {
-
-    document.documentElement.removeAttribute('data-dark-mode');
-
-  }
-
+  // }
 }
